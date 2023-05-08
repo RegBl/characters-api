9e800cd1e7c31713bd6d5496a286a6b95de2141a
Ben Nash <ben@nash.org>
2023-05-08 07:55:42 -0500

    more package name fixes

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ val logback_version: String by project
 
 plugins {
     kotlin("jvm") version "1.8.21"
-    id("regbl.ktor.plugin") version "2.3.0"
+    id("io.ktor.plugin") version "2.3.0"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.21"
 }
 
-group = "regbl.github.regbl"
+group = "io.github.regbl"
 version = "0.0.1"
 application {
-    mainClass.set("regbl.ktor.server.netty.EngineMain")
+    mainClass.set("io.ktor.server.netty.EngineMain")
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
@@ -22,12 +22,12 @@ repositories {
 }
 
 dependencies {
-    implementation("regbl.ktor:ktor-server-core-jvm:$ktor_version")
-    implementation("regbl.ktor:ktor-server-call-logging-jvm:$ktor_version")
-    implementation("regbl.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("regbl.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
-    implementation("regbl.ktor:ktor-server-netty-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-core-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-call-logging-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
-    testImplementation("regbl.ktor:ktor-server-tests-jvm:$ktor_version")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
