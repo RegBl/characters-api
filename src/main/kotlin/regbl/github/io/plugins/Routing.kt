c3d863f496a165b6c52f489dfc3d65ed5326b861
Ben Nash <ben@nash.org>
2023-05-07 21:11:06 -0500

    setup Routing.kt and CharacterRoutes.kt

--- src/main/kotlin/regbl/github/io/plugins/Routing.kt
+++ src/main/kotlin/regbl/github/io/plugins/Routing.kt
@@ -3,8 +3,10 @@ package regbl.github.io.plugins
 import io.ktor.server.routing.*
 import io.ktor.server.response.*
 import io.ktor.server.application.*
+import regbl.github.io.routes.characterRouting
 
 fun Application.configureRouting() {
     routing {
+        characterRouting()
     }
 }
--- src/main/kotlin/regbl/github/io/routes/CharacterRoutes.kt
+++ src/main/kotlin/regbl/github/io/routes/CharacterRoutes.kt
@@ -3,11 +3,18 @@ package regbl.github.io.routes
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import regbl.github.io.models.getDefaultCharacter
 
 fun Route.characterRouting() {
+    route("/") {
+        get {
+            call.respondRedirect("/character")
+        }
+    }
     route("/character") {
         get {
-            call.respondText("Hello, world!")
+            // return Character.getDefaultCharacter()
+            call.respond(getDefaultCharacter())
         }
         get("/svtpk") {
             TODO("Add handling from Save vs Total Party Kill")
